41fdbe9f42495796a33997408c3cb5d08d93a01d
viieja <msantoro@fi.uba.ar>
2021-01-09 17:02:15 -0300

    primer intento

---
+++ main.js
@@ -0,0 +1,39 @@
+const kanji_container = document.getElementById("kanji_container");
+
+
+const fetch_list = async name => {
+    const url = `https://kanjiapi.dev/v1/kanji/${name}`
+    const res = await fetch(url);
+    const lista = await res.json();
+    // console.log(lista)
+    const n_kanjis = lista.length;
+    // console.log(n_kanjis);
+    for (let i = 0; i < 10; i++) {
+        await get_kanji(lista[i]);
+    }
+}
+
+const get_kanji = async id => {
+    const url = `https://kanjiapi.dev/v1/kanji/${id}`;
+    const res = await fetch(url);
+    const kanji = await res.json();
+    console.log(kanji);
+    create_kanji_card(kanji);
+}
+
+function create_kanji_card(kanji) {
+    const kanji_div = document.createElement('div');
+    kanji_div.classList.add('kanji');
+
+    const kanjiHTML = `
+        <div class="write">
+            <h1>
+                ${kanji.kanji}
+            </h1>
+        </div>
+    `
+    kanji_div.innerHTML = kanjiHTML;
+    kanji_container.appendChild(kanji_div);
+}
+
+fetch_list("joyo");
